c69a81ac5fe2e52a60b7a91fd5c54c8622334eb5
Felipe Marelli <felipemarelli@remind101.com>
2023-06-29 17:02:26 -0300

    Como Admin, quiero poder sugerirle a un socie un máximo recomendado a pedir de retiro en ARS cada mes

--- Code.test.ts
+++ Code.test.ts
@@ -12,32 +12,34 @@ function suggested_salary_in_pesos_for_the_month(previous_salaries_during_period
         return total_salary_for_current_month;
 }
 
-test('When there is one month left, and the limit for the period cant be reached, the totality of the salary is suggested in pesos', () => {
-    let previous_salaries_during_period: Peso[] = [10, 10]
-    let total_salary_for_current_month: Peso = 10
-    let months_left_in_the_period: number = 1
-    let limit_for_the_period: Peso = 40
+describe("Suggested salary in pesos for the month", () => {
+    test('When the limit for the period cant be reached, the totality of the salary is suggested in pesos', () => {
+        let previous_salaries_during_period: Peso[] = [10, 10]
+        let total_salary_for_current_month: Peso = 10
+        let months_left_in_the_period: number = 1
+        let limit_for_the_period: Peso = 40
 
-    let suggested_salary_in_pesos = suggested_salary_in_pesos_for_the_month(
-        previous_salaries_during_period,
-        total_salary_for_current_month,
-        months_left_in_the_period,
-        limit_for_the_period
-    )
-    expect(suggested_salary_in_pesos).toBe(10);
-});
+        let suggested_salary_in_pesos = suggested_salary_in_pesos_for_the_month(
+            previous_salaries_during_period,
+            total_salary_for_current_month,
+            months_left_in_the_period,
+            limit_for_the_period
+        )
+        expect(suggested_salary_in_pesos).toBe(10);
+    });
 
-test('When there is one month left, and the limit for the period can be reached, the suggested salary is the maximum before going over the limit', () => {
-    let previous_salaries_during_period: Peso[] = [10, 10]
-    let total_salary_for_current_month: Peso = 10
-    let months_left_in_the_period: number = 1
-    let limit_for_the_period: Peso = 25
+    test('When the limit for the period can be reached, the suggested salary is the maximum before going over the limit', () => {
+        let previous_salaries_during_period: Peso[] = [10, 10]
+        let total_salary_for_current_month: Peso = 10
+        let months_left_in_the_period: number = 1
+        let limit_for_the_period: Peso = 25
 
-    let suggested_salary_in_pesos = suggested_salary_in_pesos_for_the_month(
-        previous_salaries_during_period,
-        total_salary_for_current_month,
-        months_left_in_the_period,
-        limit_for_the_period
-    )
-    expect(suggested_salary_in_pesos).toBe(5);
+        let suggested_salary_in_pesos = suggested_salary_in_pesos_for_the_month(
+            previous_salaries_during_period,
+            total_salary_for_current_month,
+            months_left_in_the_period,
+            limit_for_the_period
+        )
+        expect(suggested_salary_in_pesos).toBe(5);
+    });
 });
