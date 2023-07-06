0994cc754a5e92147a3fb1586c407c55c0d01032
Felipe Marelli <felipemarelli@remind101.com>
2023-07-06 11:21:59 -0300

    Como Admin, quiero poder sugerirle a un socie un máximo recomendado a pedir de retiro en ARS cada mes
    - Refactors and move code from tests to Code.ts

--- Code.test.ts
+++ Code.test.ts
@@ -1,21 +1,4 @@
-type Peso = number;
-
-function retiro_en_pesos_sugerido_para_el_mes(retiros_anteriores_en_el_periodo: Peso[], retiro_total_del_mes: Peso,
-                                              meses_restantes_en_el_periodo: number, maximo_de_la_ultima_categoria_monotributo: Peso): Peso {
-    let total_meses_anteriores = retiros_anteriores_en_el_periodo.reduce(
-        (total, actual) => total + actual);
-    let total_incluyendo_mes_actual = total_meses_anteriores + retiro_total_del_mes;
-
-    if (total_incluyendo_mes_actual > maximo_de_la_ultima_categoria_monotributo) {
-        return total_incluyendo_mes_actual - maximo_de_la_ultima_categoria_monotributo;
-    } else {
-        let total_meses_futuros_incluyendo_actual = maximo_de_la_ultima_categoria_monotributo - total_meses_anteriores;
-        let retiro_sugerido_por_mes_restante = total_meses_futuros_incluyendo_actual / meses_restantes_en_el_periodo
-        let retiro_sugerido_mes_actual = retiro_sugerido_por_mes_restante > retiro_total_del_mes ?
-            retiro_total_del_mes : retiro_sugerido_por_mes_restante;
-        return retiro_sugerido_mes_actual;
-    }
-}
+import {Peso, retiro_en_pesos_sugerido_para_el_mes} from "./src/Code";
 
 describe("Retiro sugerido en pesos para el mes", () => {
     test('Cuando no se va a alcanzar el limite del monotributo, todo el retiro se sugiere en pesos', () => {
--- src/Code.ts
+++ src/Code.ts
@@ -1,5 +1,22 @@
-function hello_world() {
-	console.log("hello world");
+export type Peso = number;
+
+export function retiro_en_pesos_sugerido_para_el_mes(retiros_anteriores_en_el_periodo: Peso[], retiro_total_del_mes: Peso,
+                                                     meses_restantes_en_el_periodo: number, maximo_de_la_ultima_categoria_monotributo: Peso): Peso {
+    const total_meses_anteriores = retiros_anteriores_en_el_periodo.reduce(
+        (total, actual) => total + actual);
+    const total_incluyendo_mes_actual = total_meses_anteriores + retiro_total_del_mes;
+
+    if (total_incluyendo_mes_actual > maximo_de_la_ultima_categoria_monotributo) {
+        return total_incluyendo_mes_actual - maximo_de_la_ultima_categoria_monotributo;
+    } else {
+        const total_meses_futuros_incluyendo_actual = maximo_de_la_ultima_categoria_monotributo - total_meses_anteriores;
+        const retiro_maximo_por_mes_restante = total_meses_futuros_incluyendo_actual / meses_restantes_en_el_periodo;
+        const retiro_sugerido_mes_actual = minimo(retiro_maximo_por_mes_restante, retiro_total_del_mes);
+        return retiro_sugerido_mes_actual;
+    }
 }
 
-module.exports = hello_world;
+function minimo(retiro_maximo_por_mes_restante: number, retiro_total_del_mes: number) {
+    return retiro_maximo_por_mes_restante > retiro_total_del_mes ?
+        retiro_total_del_mes : retiro_maximo_por_mes_restante;
+}
