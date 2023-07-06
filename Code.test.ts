2785d46ea7f8529e7db93b1898a62bc0fe74b5f1
Felipe Marelli <felipemarelli@remind101.com>
2023-07-06 10:20:15 -0300

    Paso el codigo a español

--- Code.test.ts
+++ Code.test.ts
@@ -1,45 +1,45 @@
 type Peso = number;
 
-function suggested_salary_in_pesos_for_the_month(previous_salaries_during_period: Peso[], total_salary_for_current_month: Peso,
-                                                 months_left_in_period: number, limit_for_the_period: Peso): Peso {
-    let total_for_previous_months = previous_salaries_during_period.reduce(
-        (sum, current) => sum + current);
-    let total_including_current_salary = total_for_previous_months + total_salary_for_current_month;
+function retiro_en_pesos_sugerido_para_el_mes(retiros_anteriores_en_el_periodo: Peso[], retiro_total_del_mes: Peso,
+                                              meses_restantes_en_el_periodo: number, limite_del_monotributo: Peso): Peso {
+    let total_meses_anteriores = retiros_anteriores_en_el_periodo.reduce(
+        (total, actual) => total + actual);
+    let total_incluyendo_mes_actual = total_meses_anteriores + retiro_total_del_mes;
 
-    if (total_including_current_salary > limit_for_the_period)
-        return total_including_current_salary - limit_for_the_period;
+    if (total_incluyendo_mes_actual > limite_del_monotributo)
+        return total_incluyendo_mes_actual - limite_del_monotributo;
     else
-        return total_salary_for_current_month;
+        return retiro_total_del_mes;
 }
 
-describe("Suggested salary in pesos for the month", () => {
-    test('When the limit for the period cant be reached, the totality of the salary is suggested in pesos', () => {
-        let previous_salaries_during_period: Peso[] = [10, 10]
-        let total_salary_for_current_month: Peso = 10
-        let months_left_in_the_period: number = 1
-        let limit_for_the_period: Peso = 40
+describe("Retiro sugerido en pesos para el mes", () => {
+    test('Cuando no se va a alcanzar el limite del monotributo, todo el retiro se sugiere en pesos', () => {
+        let retiros_anteriores_en_el_periodo: Peso[] = [10, 10]
+        let retiro_total_del_mes: Peso = 10
+        let meses_restantes_en_el_periodo: number = 1
+        let limite_del_monotributo: Peso = 40
 
-        let suggested_salary_in_pesos = suggested_salary_in_pesos_for_the_month(
-            previous_salaries_during_period,
-            total_salary_for_current_month,
-            months_left_in_the_period,
-            limit_for_the_period
+        let retiro_sugerido = retiro_en_pesos_sugerido_para_el_mes(
+            retiros_anteriores_en_el_periodo,
+            retiro_total_del_mes,
+            meses_restantes_en_el_periodo,
+            limite_del_monotributo
         )
-        expect(suggested_salary_in_pesos).toBe(10);
+        expect(retiro_sugerido).toBe(10);
     });
 
-    test('When the limit for the period can be reached, the suggested salary is the maximum before going over the limit', () => {
-        let previous_salaries_during_period: Peso[] = [10, 10]
-        let total_salary_for_current_month: Peso = 10
-        let months_left_in_the_period: number = 1
-        let limit_for_the_period: Peso = 25
+    test('Cuando se va a alcanzar el limite del monotributo, el retiro en pesos sugerido es el maximo sin pasarse', () => {
+        let retiros_anteriores_en_el_periodo: Peso[] = [10, 10]
+        let retiro_total_del_mes: Peso = 10
+        let meses_restantes_en_el_periodo: number = 1
+        let limite_del_monotributo: Peso = 25
 
-        let suggested_salary_in_pesos = suggested_salary_in_pesos_for_the_month(
-            previous_salaries_during_period,
-            total_salary_for_current_month,
-            months_left_in_the_period,
-            limit_for_the_period
+        let retiro_sugerido = retiro_en_pesos_sugerido_para_el_mes(
+            retiros_anteriores_en_el_periodo,
+            retiro_total_del_mes,
+            meses_restantes_en_el_periodo,
+            limite_del_monotributo
         )
-        expect(suggested_salary_in_pesos).toBe(5);
+        expect(retiro_sugerido).toBe(5);
     });
 });
