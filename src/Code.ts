4d514c697f2d2e1abb3a6422d46585ac35ec0d62
Felipe Marelli <felipemarelli@remind101.com>
2023-07-06 11:25:30 -0300

    Como Admin, quiero poder sugerirle a un socie un máximo recomendado a pedir de retiro en ARS cada mes
    - Refactors

--- src/Code.ts
+++ src/Code.ts
@@ -4,10 +4,10 @@ export function retiro_en_pesos_sugerido_para_el_mes(retiros_anteriores_en_el_pe
                                                      meses_restantes_en_el_periodo: number, maximo_de_la_ultima_categoria_monotributo: Peso): Peso {
     const total_meses_anteriores = retiros_anteriores_en_el_periodo.reduce(
         (total, actual) => total + actual);
-    const total_incluyendo_mes_actual = total_meses_anteriores + retiro_total_del_mes;
+    const total_meses_anteriores_incluyendo_mes_actual = total_meses_anteriores + retiro_total_del_mes;
 
-    if (total_incluyendo_mes_actual > maximo_de_la_ultima_categoria_monotributo) {
-        return total_incluyendo_mes_actual - maximo_de_la_ultima_categoria_monotributo;
+    if (total_meses_anteriores_incluyendo_mes_actual > maximo_de_la_ultima_categoria_monotributo) {
+        return total_meses_anteriores_incluyendo_mes_actual - maximo_de_la_ultima_categoria_monotributo;
     } else {
         const total_meses_futuros_incluyendo_actual = maximo_de_la_ultima_categoria_monotributo - total_meses_anteriores;
         const retiro_maximo_por_mes_restante = total_meses_futuros_incluyendo_actual / meses_restantes_en_el_periodo;
