a147e3c1d34c79208acd91692c8d556e7ac3a54e
Felipe Marelli <felipemarelli@remind101.com>
2023-07-06 11:43:03 -0300

    Como Admin, quiero poder sugerirle a un socie un máximo recomendado a pedir de retiro en ARS cada mes
    - Reemplazo "reduce" por "forEach" para que se ejecute en AppScript

--- src/Code.ts
+++ src/Code.ts
@@ -2,8 +2,7 @@ export type Peso = number;
 
 export function retiro_en_pesos_sugerido_para_el_mes(retiros_anteriores_en_el_periodo: Peso[], retiro_total_del_mes: Peso,
                                                      meses_restantes_en_el_periodo: number, maximo_de_la_ultima_categoria_monotributo: Peso): Peso {
-    const total_meses_anteriores = retiros_anteriores_en_el_periodo.reduce(
-        (total, actual) => total + actual);
+    const total_meses_anteriores = suma(retiros_anteriores_en_el_periodo);
     const total_meses_anteriores_incluyendo_mes_actual = total_meses_anteriores + retiro_total_del_mes;
 
     if (total_meses_anteriores_incluyendo_mes_actual > maximo_de_la_ultima_categoria_monotributo) {
@@ -14,6 +13,12 @@ export function retiro_en_pesos_sugerido_para_el_mes(retiros_anteriores_en_el_pe
         const retiro_sugerido_mes_actual = minimo(retiro_maximo_por_mes_restante, retiro_total_del_mes);
         return retiro_sugerido_mes_actual;
     }
+}
+
+function suma(lista_de_retiros: Peso[]): Peso {
+    let total: Peso = 0
+    lista_de_retiros.forEach(retiro => total += retiro);
+    return total;
 }
 
 function minimo(retiro_maximo_por_mes_restante: number, retiro_total_del_mes: number) {
