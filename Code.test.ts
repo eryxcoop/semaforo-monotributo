57736435c83f15d7efd887b9c4e952f8f26a6ece
Felipe Marelli <felipemarelli@remind101.com>
2023-07-26 11:36:07 -0300

    Como Admin, quiero poder sugerirle a un socie un máximo recomendado a pedir de retiro en ARS cada mes
    - Refactor tests

--- Code.test.ts
+++ Code.test.ts
@@ -68,7 +68,7 @@ describe("Retiro sugerido en pesos para el mes", () => {
     });
 
     test('Se tiene en cuenta la inflacion del mes siguiente para el retiro sugerido del mes actual', () => {
-        let retiro_total_del_mes: Peso = 20
+        let retiro_total_del_mes: Peso = 100
         let meses_restantes_en_el_periodo: number = 2
         let maximo_de_la_ultima_categoria_monotributo: Peso = 40
         let inflacion_proyectada_mensual = 0.5;
@@ -80,26 +80,29 @@ describe("Retiro sugerido en pesos para el mes", () => {
             maximo_de_la_ultima_categoria_monotributo,
             inflacion_proyectada_mensual
         )
-        let inflacion_efectiva_futura = inflacion_proyectada_mensual
-        let xxx = meses_restantes_en_el_periodo + inflacion_efectiva_futura;
-        let maximo_retiro_actual_considerando_inflacion_futura = maximo_de_la_ultima_categoria_monotributo / xxx
+        let factor_de_inflacion = inflacion_proyectada_mensual + 1
+        let inflacion_acumulada_en_el_periodo = 1 + factor_de_inflacion
+        let maximo_retiro_actual_considerando_inflacion_futura = maximo_de_la_ultima_categoria_monotributo / inflacion_acumulada_en_el_periodo;
         expect(retiro_sugerido).toBe(maximo_retiro_actual_considerando_inflacion_futura);
+
     });
 
     test('La inflacion se estima correctamente para varios meses futuros', () => {
-        let retiros_anteriores_en_el_periodo: Peso[] = []
-        let retiro_total_del_mes: Peso = 10
+        let retiro_total_del_mes: Peso = 100
         let meses_restantes_en_el_periodo: number = 3
         let maximo_de_la_ultima_categoria_monotributo: Peso = 38
         let inflacion_proyectada_mensual = 0.5;
 
         let retiro_sugerido = retiro_en_pesos_sugerido_para_el_mes(
-            retiros_anteriores_en_el_periodo,
+            [],
             retiro_total_del_mes,
             meses_restantes_en_el_periodo,
             maximo_de_la_ultima_categoria_monotributo,
             inflacion_proyectada_mensual
         )
-        expect(retiro_sugerido).toBe(8);
+        let factor_de_inflacion = inflacion_proyectada_mensual + 1
+        let inflacion_acumulada_en_el_periodo = 1 + factor_de_inflacion + Math.pow(factor_de_inflacion, 2)
+        let maximo_retiro_actual_considerando_inflacion_futura = maximo_de_la_ultima_categoria_monotributo / inflacion_acumulada_en_el_periodo;
+        expect(retiro_sugerido).toBe(maximo_retiro_actual_considerando_inflacion_futura);
     });
 });
