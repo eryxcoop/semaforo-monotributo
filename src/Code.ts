2587d044f2b5a013e0a1a0df3b898a88a721410d
Felipe Marelli <felipemarelli@remind101.com>
2023-08-17 12:44:58 -0300

    Como Admin, quiero contemplar que lo que no se pide en ARS es un monto que se va a pedir en USD y eso también suma para el límite de monotributo.

--- src/Code.ts
+++ src/Code.ts
@@ -11,9 +11,9 @@ export function retiro_en_pesos_sugerido_para_el_mes(
 ): Peso {
     const total_restante_en_el_periodo = restante_en_el_periodo_antes_de_subir_de_categoria(retiros_anteriores_en_el_periodo,
         maximo_de_la_ultima_categoria_monotributo);
-    if(total_restante_en_el_periodo <= 0) {
+    if (ya_se_paso_el_maximo_del_monotributo(total_restante_en_el_periodo))
         throw 'Ya se paso el limite de la ultima categoria del monotributo en el pasado';
-    }
+
 
     const inflacion_acumulada_futura = inflacion_acumulada_futura_en_el_periodo(meses_restantes_en_el_periodo, inflacion_proyectada_mensual);
 
@@ -25,6 +25,10 @@ export function retiro_en_pesos_sugerido_para_el_mes(
     const retiro_sugerido_mes_actual = minimo(retiro_maximo_mes_actual, retiro_total_del_mes);
 
     return retiro_sugerido_mes_actual;
+}
+
+function ya_se_paso_el_maximo_del_monotributo(total_restante_en_el_periodo: number) {
+    return total_restante_en_el_periodo <= 0;
 }
 
 function restante_en_el_periodo_antes_de_subir_de_categoria(retiros_anteriores_en_el_periodo: Peso[], maximo_de_la_ultima_categoria_monotributo: number) {
