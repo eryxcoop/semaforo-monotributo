ba58e85e6adc4180aa1f2a02fb0d0cd0547acaec
Felipe Marelli <felipemarelli@remind101.com>
2023-08-25 09:51:32 -0300

    Convertir codigo a una clase

--- src/Code.ts
+++ src/Code.ts
@@ -29,7 +29,7 @@ export class Xxx {
         const total_restante_en_el_periodo =
             restante_en_el_periodo_antes_de_subir_de_categoria(this.retiros_anteriores_en_el_periodo, this.maximo_de_la_ultima_categoria_monotributo);
 
-        if (ya_se_paso_el_maximo_del_monotributo(total_restante_en_el_periodo))
+        if (this.ya_se_paso_el_maximo_del_monotributo(total_restante_en_el_periodo))
             throw 'Ya se paso el limite de la ultima categoria del monotributo en el pasado';
 
         const inflacion_acumulada_futura = inflacion_acumulada_futura_en_el_periodo(this.meses_restantes_en_el_periodo, this.inflacion_proyectada_mensual);
@@ -43,37 +43,10 @@ export class Xxx {
 
         return retiro_sugerido_mes_actual;
     }
-}
 
-export function retiro_en_pesos_sugerido_para_el_mes(
-    retiros_anteriores_en_el_periodo: Peso[],
-    retiro_total_del_mes: Peso,
-    meses_restantes_en_el_periodo: number,
-    maximo_de_la_ultima_categoria_monotributo: Peso,
-    inflacion_proyectada_mensual,
-    precio_del_dolar_oficial_actual,
-    precio_del_dolar_mep_actual,
-): Peso {
-    const total_restante_en_el_periodo =
-        restante_en_el_periodo_antes_de_subir_de_categoria(retiros_anteriores_en_el_periodo, maximo_de_la_ultima_categoria_monotributo);
-
-    if (ya_se_paso_el_maximo_del_monotributo(total_restante_en_el_periodo))
-        throw 'Ya se paso el limite de la ultima categoria del monotributo en el pasado';
-
-    const inflacion_acumulada_futura = inflacion_acumulada_futura_en_el_periodo(meses_restantes_en_el_periodo, inflacion_proyectada_mensual);
-
-    if (precio_del_dolar_oficial_actual != precio_del_dolar_mep_actual) {
-        return 6;
+    ya_se_paso_el_maximo_del_monotributo(total_restante_en_el_periodo: number) {
+        return total_restante_en_el_periodo <= 0;
     }
-
-    const retiro_maximo_mes_actual = total_restante_en_el_periodo / inflacion_acumulada_futura;
-    const retiro_sugerido_mes_actual = minimo(retiro_maximo_mes_actual, retiro_total_del_mes);
-
-    return retiro_sugerido_mes_actual;
-}
-
-function ya_se_paso_el_maximo_del_monotributo(total_restante_en_el_periodo: number) {
-    return total_restante_en_el_periodo <= 0;
 }
 
 function restante_en_el_periodo_antes_de_subir_de_categoria(retiros_anteriores_en_el_periodo: Peso[], maximo_de_la_ultima_categoria_monotributo: number) {
